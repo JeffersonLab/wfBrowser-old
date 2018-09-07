f1a74141b8b111b6ff374bb22883a5c8ebd82d7c
Adam Carpenter <adamc@jlab.org>
2018-09-07 14:24:33 -0400

    Add equals, hashcode, and toString methods used in testing

--- src/java/org/jlab/wfbrowser/model/Event.java
+++ src/java/org/jlab/wfbrowser/model/Event.java
@@ -6,25 +6,56 @@
 package org.jlab.wfbrowser.model;
 
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.Objects;
 
 /**
+ * Object for representing and waveform triggering event. The eventId is
+ * optional as an Event object needs to be created before it is added to the
+ * database, and the eventId is only created in the database once the event is
+ * added. While not strictly enforced, an eventId value of null should imply
+ * that the Event object was not read from the database. Since the database can
+ * only handle microsecond resolution, we truncate event times to that here.
+ * This helps with testing.
  *
  * @author adamc
  */
 public class Event {
+
+    private Long eventId = null;
     private final Instant eventTime;
     private final String location;
     private final String system;
     private final boolean archive;
     private final List<Waveform> waveforms;
 
-    public Event(Instant eventTime, String location, String system, boolean archive, List<Waveform> waveforms) {
-        this.eventTime = eventTime;
+    public Event(long eventId, Instant eventTime, String location, String system, boolean archive, List<Waveform> waveforms) {
+        this.eventId = eventId;
+        this.eventTime = eventTime.truncatedTo(ChronoUnit.MICROS);
         this.location = location;
         this.system = system;
         this.archive = archive;
         this.waveforms = waveforms;
+    }
+
+    public Event(Instant eventTime, String location, String system, boolean archive, List<Waveform> waveforms) {
+        this.eventTime = eventTime.truncatedTo(ChronoUnit.MICROS);
+        this.location = location;
+        this.system = system;
+        this.archive = archive;
+        this.waveforms = waveforms;
+    }
+
+    public Long getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(Long eventId) {
+        this.eventId = eventId;
     }
 
     public Instant getEventTime() {
@@ -46,5 +77,52 @@ public class Event {
     public List<Waveform> getWaveforms() {
         return waveforms;
     }
-    
+
+    public String getEventTimeString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS").withZone(ZoneOffset.UTC);
+        return formatter.format(eventTime);
+    }
+
+    /**
+     * Events are considered equal if all of the metadata about the event are
+     * equal. We currently do not enforce equality of waveform data as the
+     * database should maintain uniqueness of event IDs and it's related data.
+     *
+     * @param o
+     * @return
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (!(o instanceof Event)) {
+            return false;
+        }
+
+        Event e = (Event) o;
+        return eventId == null ? null == e.getEventId() : eventId.equals(e.getEventId()) && eventTime.equals(e.getEventTime()) && location.equals(e.getLocation()) && system.equals(e.getSystem())
+                && archive == e.isArchive() && waveforms.size() == e.getWaveforms().size();
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 11 * hash + Objects.hashCode(this.eventId);
+        hash = 11 * hash + Objects.hashCode(this.eventTime);
+        hash = 11 * hash + Objects.hashCode(this.location);
+        hash = 11 * hash + Objects.hashCode(this.system);
+        hash = 11 * hash + (this.archive ? 1 : 0);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        String wData = "";
+        for(Waveform w: waveforms) {
+            wData = wData + w.toString() + "\n";
+        }
+        return "eventId: " + eventId + "\neventTime: " + getEventTimeString() + "\nlocation: " + location + "\nsystem: " + system
+                + "\nnum Waveforms: " + waveforms.size() + "\nWaveform Data:\n" + wData;
+    }
 }
--- src/java/org/jlab/wfbrowser/model/Waveform.java
+++ src/java/org/jlab/wfbrowser/model/Waveform.java
@@ -5,28 +5,59 @@
  */
 package org.jlab.wfbrowser.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- *
+ * This an object just to represent waveform data.  The object can be instantiated with or without supplying data, and supports
+ * adding timeOffset/value pairs.  Should be used in conjunction with a "parent" Event object that contains information system
+ * location, and waveform trigger "event" time.
  * @author adamc
  */
 public class Waveform {
-    private double[] timeOffsets;
-    private double[] values;
+    private final String seriesName;
+
+    public String getSeriesName() {
+        return seriesName;
+    }
+    private final List<Double> timeOffsets = new ArrayList<>();
+    private final  List<Double> values = new ArrayList<>();
     
-    public Waveform(double[] timeOffsets, double[] values) {
-        if (timeOffsets.length != values.length) {
+    public Waveform(String seriesName, List<Double> timeOffsets, List<Double> values) {
+        if (timeOffsets.size() != values.size()) {
             throw new IllegalArgumentException("time and value arrays are of unequal length");
         }
-        this.timeOffsets = timeOffsets;
-        this.values = values;
+        this.seriesName = seriesName;
+        this.timeOffsets.addAll(timeOffsets);
+        this.values.addAll(values);
     }
 
-    public double[] getTimeOffsets() {
+    public Waveform(String seriesName) {
+        this.seriesName = seriesName;
+    }
+    
+    public List<Double> getTimeOffsets() {
         return timeOffsets;
     }
 
-    public double[] getValues() {
+    public List<Double> getValues() {
         return values;
     }
     
+    public void addPoint(Double timeOffset, Double value) {
+        if (timeOffset == null || value == null) {
+            throw new IllegalArgumentException("Attempting to add point to waveform with null value");
+        }
+        timeOffsets.add(timeOffset);
+        values.add(value);
+    }
+    
+    public String toString() {
+        String out = "seriesName: " + seriesName + "\npoints: {";
+        for(int i = 0; i < timeOffsets.size(); i++) {
+            out = out + " [" + timeOffsets.get(i) + "," + values.get(i) + "]";
+        }
+        out = out + "}";
+        return out;
+    }
 }
