fec01e3a7ce508e989ff58c1a3ef8bca82f1818d
Adam Carpenter <adamc@jlab.org>
2018-09-11 12:46:54 -0400

    Add toJsonObject method

--- src/java/org/jlab/wfbrowser/model/Event.java
+++ src/java/org/jlab/wfbrowser/model/Event.java
@@ -6,12 +6,16 @@
 package org.jlab.wfbrowser.model;
 
 import java.time.Instant;
-import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Objects;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import org.jlab.wfbrowser.business.util.TimeUtil;
 
 /**
  * Object for representing and waveform triggering event. The eventId is
@@ -78,6 +82,38 @@ public class Event {
         return waveforms;
     }
 
+    /**
+     * Generate a json object representing an event. Only include a waveforms
+     * parameter/array if the waveforms list isn't null. Only use this method if
+     * you're returning a Event that came from the data or has an associated
+     * database event_id value, since it doesn't make any sense to hand out
+     * "unofficial" data through one of our data API end points.
+     *
+     * @return
+     */
+    public JsonObject toJsonObject() {
+        JsonObjectBuilder job = Json.createObjectBuilder();
+        if (eventId != null) {
+            job.add("id", eventId)
+                    .add("datetime", TimeUtil.getDateTimeString(eventTime))
+                    .add("location", location)
+                    .add("system", system)
+                    .add("archive", archive);
+            if (waveforms != null) {
+                // Don't add a waveforms parameter if it's null.  That indicates that the waveforms were requested
+                JsonArrayBuilder jab = Json.createArrayBuilder();
+                for (Waveform w : waveforms) {
+                    jab.add(w.toJsonObject());
+                }
+                job.add("waveforms", jab.build());
+            }
+        } else {
+            // Should never try to send out a response on an "Event" that didn't come from the database.  Full stop if we try.
+            throw new RuntimeException("Cannot return event without database event ID");
+        }
+        return job.build();
+    }
+
     public String getEventTimeString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS").withZone(ZoneOffset.UTC);
         return formatter.format(eventTime);
@@ -85,8 +121,9 @@ public class Event {
 
     /**
      * Events are considered equal if all of the metadata about the event are
-     * equal. We currently do not enforce equality of waveform data as the
-     * database should maintain uniqueness of event IDs and it's related data.
+     * equal. We currently do not enforce equality of waveform data, only that
+     * an event has the same number of waveforms. The database should maintain
+     * uniqueness of event IDs and it's related data.
      *
      * @param o
      * @return
@@ -101,8 +138,20 @@ public class Event {
         }
 
         Event e = (Event) o;
-        return eventId == null ? null == e.getEventId() : eventId.equals(e.getEventId()) && eventTime.equals(e.getEventTime()) && location.equals(e.getLocation()) && system.equals(e.getSystem())
-                && archive == e.isArchive() && waveforms.size() == e.getWaveforms().size();
+        boolean isEqual = true;
+        isEqual = isEqual && (eventId == null ? e.getEventId() == null : eventId.equals(e.getEventId()));
+        if (e.getWaveforms() != null || waveforms != null) {
+            if (e.getWaveforms() == null || waveforms == null) {
+                isEqual = false;
+            } else {
+                isEqual = isEqual && (waveforms.size() == e.getWaveforms().size());
+            }
+        }
+        isEqual = isEqual && (eventTime.equals(e.getEventTime()));
+        isEqual = isEqual && (location.equals(e.getLocation()));
+        isEqual = isEqual && (system.equals(e.getSystem()));
+        isEqual = isEqual && (archive == e.isArchive());
+        return isEqual;
     }
 
     @Override
