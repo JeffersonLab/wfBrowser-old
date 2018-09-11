255573c2f74f3f30d0cead6426077536aa166743
Adam Carpenter <adamc@jlab.org>
2018-09-11 12:46:43 -0400

    Add toJsonObject method

--- src/java/org/jlab/wfbrowser/model/Waveform.java
+++ src/java/org/jlab/wfbrowser/model/Waveform.java
@@ -7,22 +7,30 @@ package org.jlab.wfbrowser.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 
 /**
- * This an object just to represent waveform data.  The object can be instantiated with or without supplying data, and supports
- * adding timeOffset/value pairs.  Should be used in conjunction with a "parent" Event object that contains information system
- * location, and waveform trigger "event" time.
+ * This an object just to represent waveform data. The object can be
+ * instantiated with or without supplying data, and supports adding
+ * timeOffset/value pairs. Should be used in conjunction with a "parent" Event
+ * object that contains information system location, and waveform trigger
+ * "event" time.
+ *
  * @author adamc
  */
 public class Waveform {
+
     private final String seriesName;
 
     public String getSeriesName() {
         return seriesName;
     }
     private final List<Double> timeOffsets = new ArrayList<>();
-    private final  List<Double> values = new ArrayList<>();
-    
+    private final List<Double> values = new ArrayList<>();
+
     public Waveform(String seriesName, List<Double> timeOffsets, List<Double> values) {
         if (timeOffsets.size() != values.size()) {
             throw new IllegalArgumentException("time and value arrays are of unequal length");
@@ -35,7 +43,7 @@ public class Waveform {
     public Waveform(String seriesName) {
         this.seriesName = seriesName;
     }
-    
+
     public List<Double> getTimeOffsets() {
         return timeOffsets;
     }
@@ -43,7 +51,7 @@ public class Waveform {
     public List<Double> getValues() {
         return values;
     }
-    
+
     public void addPoint(Double timeOffset, Double value) {
         if (timeOffset == null || value == null) {
             throw new IllegalArgumentException("Attempting to add point to waveform with null value");
@@ -51,13 +59,29 @@ public class Waveform {
         timeOffsets.add(timeOffset);
         values.add(value);
     }
-    
+
     public String toString() {
         String out = "seriesName: " + seriesName + "\npoints: {";
-        for(int i = 0; i < timeOffsets.size(); i++) {
+        for (int i = 0; i < timeOffsets.size(); i++) {
             out = out + " [" + timeOffsets.get(i) + "," + values.get(i) + "]";
         }
         out = out + "}";
         return out;
+    }
+
+    public JsonObject toJsonObject() {
+        JsonObjectBuilder wjob = Json.createObjectBuilder()
+                .add("series", seriesName);
+        JsonArrayBuilder tjab = Json.createArrayBuilder();
+        for (Double t : timeOffsets) {
+            tjab.add(t);
+        }
+        wjob.add("timeOffsets", tjab.build());
+        JsonArrayBuilder vjab = Json.createArrayBuilder();
+        for (Double v : values){
+            vjab.add(v);
+        }
+        wjob.add("values", vjab.build());
+        return wjob.build();
     }
 }
