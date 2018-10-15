c60a5784dad557038a32227e7b2911264ac04d30
Adam Carpenter <adamc@jlab.org>
2018-10-15 13:47:52 -0400

    Fix SQL syntax error

--- src/java/org/jlab/wfbrowser/business/filter/EventFilter.java
+++ src/java/org/jlab/wfbrowser/business/filter/EventFilter.java
@@ -95,7 +95,7 @@ public class EventFilter {
         }
 
         if (!filters.isEmpty()) {
-            filter = "WHERE " + filters.get(0);
+            filter = " WHERE " + filters.get(0);
 
             if (filters.size() > 1) {
                 for (int i = 1; i < filters.size(); i++) {
