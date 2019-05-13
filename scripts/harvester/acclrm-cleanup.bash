a034b832ef138d3c8bf26a1a060bdc59bec4eedf
Adam Carpenter <adamc@jlab.org>
2019-05-13 07:56:20 -0400

    Removed print option from find

--- scripts/harvester/acclrm-cleanup.bash
+++ scripts/harvester/acclrm-cleanup.bash
@@ -27,7 +27,6 @@ AGE="+120"
   -maxdepth 1 \
   -type f \
   -mtime ${AGE} \
-  -print \
   -execdir /bin/bash -c 'if [ ! -e '{}.tar.gz' ] ; then /bin/tar -czf '{}.tar.gz' '{}'; else /bin/false ; fi' \; \
   -execdir /bin/rm -f {} \;
 
