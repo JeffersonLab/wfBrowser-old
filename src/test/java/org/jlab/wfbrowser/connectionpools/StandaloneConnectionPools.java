04f74d807504adb01c50f1ad6efdad2e21dbb827
Adam Carpenter <adamc@jlab.org>
2023-08-22 13:54:32 -0400

    Switch to MariaDB DS and one-time creation

--- src/test/java/org/jlab/wfbrowser/connectionpools/StandaloneConnectionPools.java
+++ src/test/java/org/jlab/wfbrowser/connectionpools/StandaloneConnectionPools.java
@@ -1,44 +1,51 @@
 package org.jlab.wfbrowser.connectionpools;
 
-import com.mysql.cj.jdbc.MysqlConnectionPoolDataSource;
 import java.io.IOException;
-import java.nio.channels.Channel;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
-import org.apache.commons.dbcp2.datasources.SharedPoolDataSource;
+import org.mariadb.jdbc.MariaDbPoolDataSource;
 
 /**
- * Stolen and modified from the jmya API test suite. Updated to use newer
- * Connector/J. Allows use of the PooledNexus when outside of a Java application
- * server such as Tomcat.
+ * JNDI DataSources are required the SqlUtil class to operate and generally
+ * these are configured inside an application server such as Tomcat or Wildfly.
+ * This class creates DataSources using the MariaDbPoolDataSource
+ * (https://mariadb.com/kb/en/pool-datasource-implementation/).
  *
- * JNDI DataSources are required for the PooledNexus to operate and generally
- * these are configured inside of an application server such as Tomcat. This
- * class creates DataSources using the DBCP pooling library to fill the role of
- * an application server when there isn't one configured.
- *
- * @author slominskir
+ * @author adamc
+ * @implNote I found that the tests would run into problems when multiple datasources had been created and closed.  The
+ * second datasource would not open properly (maybe the first didn't really close on the database side?).  This now
+ * creates a single database pool and uses it throughout the tests.  The tests should not try to close the database
+ * pool connection as it will terminate with the tests.
  */
-public class StandaloneConnectionPools implements Channel {
+public class StandaloneConnectionPools {
 
-    private final InitialContext initCtx;
-    private final Context envCtx;
-    private final List<SharedPoolDataSource> dsList = new ArrayList<>();
+    private static final InitialContext initCtx;
+    private static final Context envCtx;
+
+    static {
+        new StandaloneJndi();
+        try {
+            initCtx = new InitialContext();
+            envCtx = (Context) initCtx.lookup("java:comp/env");
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static MariaDbPoolDataSource ds = null;
+    private static String ds_name = "jdbc/waveforms_rw";
 
     /**
-     * Creates DataSources and publishes them to JNDI.
-     *
-     * @throws javax.naming.NamingException
-     * @throws java.sql.SQLException
+     * This can be called as many times as desired.  It only has effect if no data source exists.
+     * @throws NamingException
+     * @throws SQLException
      */
-    public StandaloneConnectionPools() throws NamingException, SQLException {
-        initCtx = new InitialContext();
-        envCtx = (Context) initCtx.lookup("java:comp/env");
-
+    public static void setupConnectionPool() throws NamingException, SQLException {
+        if (ds != null) {
+            return;
+        }
         String user = "wftest_writer";
         String password = "password";
 
@@ -46,71 +53,33 @@ public class StandaloneConnectionPools implements Channel {
         int port = 3306;
         String host = "localhost";
 
-        String url = "jdbc:mysql://" + host + ":" + port + "/waveformstest";
+        String url = "jdbc:mariadb://" + host + ":" + port + "/waveformstest?user=" + user + "&password=" + password
+                + "&maxIdleTime=60";
 
-        MysqlConnectionPoolDataSource pds = new MysqlConnectionPoolDataSource();
-        pds.setUrl(url);
-        pds.setUser(user);
-        pds.setPassword(password);
-
-        pds.setUseCompression(true);
-        pds.setNoAccessToProcedureBodies(true);
-
-        // Specific to Connections from a pool
-        pds.setCacheCallableStmts(true);
-        pds.setCachePrepStmts(true);
-
-        pds.setPrepStmtCacheSqlLimit(1024); // I assume this is max length of SQL query string
-
-        pds.setPrepStmtCacheSize(1024); // Each PV table requires a separate stmt
-        pds.setCallableStmtCacheSize(8);
-
-        pds.setServerTimezone("America/New_York");
-        
-        // These look interesting, but can't find any docs on them
-        //pds.setUseServerPreparedStmts(true); // Should be true by default?
-        //pds.setAutoClosePStmtStreams(true);
-        //pds.setAutoReconnect(true);
-        //pds.setConnectTimeout(1000);
-        //pds.setEnablePacketDebug(true);
-        //pds.setDumpQueriesOnException(true);
-        //pds.setGatherPerfMetrics(true);
-        //pds.setMaxAllowedPacket(1000);
-        //pds.setUseStreamLengthsInPrepStmts(true);
-        //pds.setUltraDevHack(true);
-        //pds.setStrictFloatingPoint(true);
-        //pds.setSlowQueryThresholdMillis(1000);
-        //pds.setResultSetSizeThreshold(1000);
-        //pds.setNetTimeoutForStreamingResults(1000);
-        SharedPoolDataSource ds = new SharedPoolDataSource();
-        ds.setConnectionPoolDataSource(pds);
-
-        ds.setMaxTotal(20); // Max connections in the pool (regardless of idle vs active)
-
-        envCtx.rebind("jdbc/waveforms_rw", ds);
-        dsList.add(ds);
+        ds = new MariaDbPoolDataSource(url);
+        envCtx.rebind(ds_name, ds);
     }
 
-    @Override
-    public boolean isOpen() {
-        return !dsList.isEmpty();
-    }
+    /**
+     * Creates DataSources and publishes them to JNDI.
+     *
+     * @throws javax.naming.NamingException
+     * @throws java.sql.SQLException
+     */
+    private StandaloneConnectionPools() {}
 
-    @Override
-    public void close() throws IOException {
-        int errorCount = 0;
-        for (SharedPoolDataSource ds : dsList) {
-            try {
-                ds.close();
-            } catch (Exception e) {
-                errorCount++;
-            }
+    /**
+     * Don't call this.  Seems like the datasource does not close in a way that allows a new one to be created/setup.
+     * I left it here as a reference for what tearDown would look like.
+     * @throws IOException
+     */
+    public static void tearDownConnectionPool() throws IOException {
+        try {
+            ds.close();
+            envCtx.unbind(ds_name);
+            ds = null;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-
-        if (errorCount > 0) {
-            throw new IOException("Unable to close");
-        }
-
-        dsList.clear();
     }
 }
